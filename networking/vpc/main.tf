8de518b0e5f37aa943e4c24c2561dee9d6883e56
Giuseppe Ferri <gferri.dev@gmail.com>
2025-05-17 18:16:30 -0300

    add outputs

--- networking/vpc/main.tf
+++ networking/vpc/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_subnet" "public" {
@@ -29,4 +31,20 @@ resource "aws_security_group" "db_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.db.name
+}
+
+output "security_group_id" {
+  value = aws_security_group.db_sg.id
 }
